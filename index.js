9694a1adea717d14681dbf48a99e89c476f367c0
Theara123422 <theara6574@gmail.com>
2024-12-03 18:13:20 +0700

    Finalize

--- index.js
+++ index.js
@@ -27,14 +27,14 @@ getClock();
 setInterval(getClock, 1000);
 
 const obj = [
-  { name: "Tony", price: 10.99, img: "flash-sale/flash-sale1.avif" },
-  { name: "Lucky", price: 12.99, img: "flash-sale/flash-sale2.avif" },
-  { name: "Predo", price: 11.99, img: "flash-sale/flash-sale3.avif" },
-  { name: "Koko", price: 8.99, img: "flash-sale/flash-sale4.avif" },
-  { name: "A Lok", price: 15.99, img: "flash-sale/flash-sale5.avif" },
-  { name: "July", price: 17.99, img: "flash-sale/flash-sale6.avif" },
-  { name: "A Kom Sot", price: 17.99, img: "flash-sale/flash-sale7.avif" },
-  { name: "Momo", price: 12.99, img: "flash-sale/flash-sale8.avif" },
+  { name: "SAMSUNG 27 Essential S3 (S36GD) Series FHD 1800R Curved Computer Monitor, 100Hz, Game Mode, Advanced Eye Comfort", price: 10.99, img: "flash-sale/flash-sale1.avif" },
+  { name: "SAMSUNG 24-Inch Odyssey G3 (G30D) Series FHD Gaming Monitor, 1ms, 180Hz, AMD FreeSync, Adjustable Stand, Black Equalizer, Virtual Aim Point", price: 12.99, img: "flash-sale/flash-sale2.avif" },
+  { name: "ASUS Vivobook S 14 OLED Laptop, Intel Core Ultra 9 185H, 16GB, 1TB SSD, Neutral Black, S5406MA-AS96, Intel Evo Edition", price: 11.99, img: "flash-sale/flash-sale3.avif" },
+  { name: "Dell Optiplex 7050 SFF Desktop PC Intel i7-7700 4-Cores 3.60GHz 32GB DDR4 1TB SSD WiFi BT HDMI Duel Monitor Support Windows 10 Pro Excellent Condition(Renewed)", price: 8.99, img: "flash-sale/flash-sale4.avif" },
+  { name: "EVGA X12 Gaming Mouse, 8k, Wired, White, Customizable, Dual Sensor, 16,000 DPI, 5 Profiles", price: 15.99, img: "flash-sale/flash-sale5.avif" },
+  { name: "Sennheiser Consumer Audio HD 650 - Audiophile Hi-Res Open Back Dynamic Headphone", price: 17.99, img: "flash-sale/flash-sale6.avif" },
+  { name: "Samsung 32 UJ59 Series 4K UHD (3840x2160) Computer Monitor", price: 17.99, img: "flash-sale/flash-sale7.avif" },
+  { name: "Samsung Galaxy Tab S6 Lite (2024)", price: 12.99, img: "flash-sale/flash-sale8.avif" },
 ];
 
 let index = 0;
